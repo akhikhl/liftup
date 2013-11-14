941327cb12d6e54c8b2e511db43ae76c71fc3bbd
Andrey Hihlovskiy <akhikhl@gmail.com>
2013-11-14 09:19:42 +0100

    optimized gradle configuration

---
+++ buildSrc/src/main/java/sweethelpers/IConsole.java
@@ -0,0 +1,12 @@
+package sweethelpers;
+
+public interface IConsole {
+
+  void endProgress();
+
+  void info(String message);
+
+  void progress(String message);
+
+  void startProgress(String message);
+}
