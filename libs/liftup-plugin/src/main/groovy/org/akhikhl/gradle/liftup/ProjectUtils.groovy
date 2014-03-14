f46f8390b79a885883b81c84a576d409bedca542
Andrey Hihlovskiy <akhikhl@gmail.com>
2014-03-14 08:37:06 +0100

    introduced method ProjectUtils.findBundleActivator

--- libs/liftup-plugin/src/main/groovy/org/akhikhl/gradle/liftup/ProjectUtils.groovy
+++ libs/liftup-plugin/src/main/groovy/org/akhikhl/gradle/liftup/ProjectUtils.groovy
@@ -1,8 +1,28 @@
 package org.akhikhl.gradle.liftup
 
+import java.nio.file.Paths
+
 import org.gradle.api.Project
 
 final class ProjectUtils {
+
+  static String findBundleActivator(Project project) {
+    String activator
+    project.sourceSets.main.allSource.srcDirs.findResult { File srcDir ->
+      project.fileTree(srcDir).include('**/Activator.groovy').files.findResult { File activatorSourceFile ->
+        activator = Paths.get(srcDir.absolutePath).relativize(Paths.get(activatorSourceFile.absolutePath)).toString()
+        activator = activator.substring(0, activator.length() - 7) // remove '.groovy' file extension
+        activator = activator.replaceAll('/', '.') // convert to package.class
+      }
+      if(!activator)
+        project.fileTree(srcDir).include('**/Activator.java').files.findResult { File activatorSourceFile ->
+          activator = Paths.get(srcDir.absolutePath).relativize(Paths.get(activatorSourceFile.absolutePath)).toString()
+          activator = activator.substring(0, activator.length() - 5) // remove '.java' file extension
+          activator = activator.replaceAll('/', '.') // convert to package.class
+        }
+      activator
+    }
+  }
 
   static findUpAncestorChain(Project project, Closure condition) {
     Project p = project
