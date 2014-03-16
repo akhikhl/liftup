c77e7cdd3304420ce3bcf84f64f73beaa4fb2bea
Andrey Hihlovskiy <akhikhl@gmail.com>
2014-03-16 09:27:09 +0100

    added groovydoc to ProjectUtils

--- libs/liftup-plugin/src/main/groovy/org/akhikhl/gradle/liftup/ProjectUtils.groovy
+++ libs/liftup-plugin/src/main/groovy/org/akhikhl/gradle/liftup/ProjectUtils.groovy
@@ -6,6 +6,12 @@ import org.gradle.api.Project
 
 final class ProjectUtils {
 
+  /**
+   * Finds bundle activator.
+   *
+   * @param project project being analyzed, not modified.
+   * @return qualified name (package.class) of the bundle activator, if present.
+   */
   static String findBundleActivator(Project project) {
     String activator
     project.sourceSets.main.allSource.srcDirs.findResult { File srcDir ->
@@ -24,26 +30,34 @@ final class ProjectUtils {
     }
   }
 
-  static findPluginConfig(Project project) {
-    def pluginFile = findPluginConfigFile(project)
-    pluginFile ? new XmlParser().parse(pluginFile) : null
-  }
-
-  static findPluginConfigFile(Project project) {
-    project.sourceSets.main.resources.srcDirs.findResult { File srcDir ->
-      def pluginFile = new File(srcDir, 'plugin.xml')
+  /**
+   * Finds eclipse plugin configuration file, 'plugin.xml'.
+   *
+   * @param project project being analyzed, not modified.
+   * @return java.io.File, pointing to 'plugin.xml', or null, if configuration file does not exist.
+   */
+  static File findPluginConfigFile(Project project) {
+    ([project.projectDir] + project.sourceSets.main.resources.srcDirs).findResult { File dir ->
+      File pluginFile = new File(dir, 'plugin.xml')
       pluginFile.exists() ? pluginFile : null
     }
   }
 
-  static findPluginLocalizationFiles(Project project) {
-    project.sourceSets.main.resources.srcDirs.findResult { File srcDir ->
-      if(srcDir.exists()) {
-        File locatizationDir = new File(srcDir, 'OSGI-INF/l10n')
+  /**
+   * Finds eclipse plugin configuration localization files, 'plugin*.properties'.
+   *
+   * @param project project being analyzed, not modified.
+   * @return list of strings (absolute paths) to plugin configuration localization files
+   * or null, if no localization files are found.
+   */
+  static List<String> findPluginLocalizationFiles(Project project) {
+    ([project.projectDir] + project.sourceSets.main.resources.srcDirs).findResult { File dir ->
+      if(dir.exists()) {
+        File locatizationDir = new File(dir, 'OSGI-INF/l10n')
         // if locatizationDir exists, it will be used by OSGi automatically
         // and there's no need for Bundle-Localization
         if(!locatizationDir.exists()) {
-          def localizationFiles = new FileNameFinder().getFileNames(srcDir.absolutePath, 'plugin*.properties')
+          List<String> localizationFiles = new FileNameFinder().getFileNames(dir.absolutePath, 'plugin*.properties')
           if(localizationFiles)
             return localizationFiles
         }
@@ -52,15 +66,29 @@ final class ProjectUtils {
     }
   }
 
-  static findUpAncestorChain(Project project, Closure condition) {
+  /**
+   * Finds a first project satisfying the given condition in the given ancestor chain.
+   *
+   * @param project project being analyzed, not modified.
+   * @param condition closure which is repeatedly called against every project in the ancestor chain.
+   *   If closure returns "truthy" value, the condition is satisfied and iteration breaks.
+   * @return first project satisfying the given condition.
+   */
+  static Project findUpAncestorChain(Project project, Closure condition) {
     Project p = project
     while(p != null && !condition(p))
       p = p.parent
     return p
   }
 
-  static List withAllAncestors(Project project) {
-    List projects = []
+  /**
+   * Returns the list of all ancestors + the given project.
+   *
+   * @param project project being analyzed, not modified.
+   * @return list of projects, first element is root, last element is the given project.
+   */
+  static List<Project> withAllAncestors(Project project) {
+    List<Project> projects = []
     Project p = project
     while(p != null) {
       projects.add(0, p)
@@ -69,4 +97,3 @@ final class ProjectUtils {
     return projects
   }
 }
-
